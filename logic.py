9409b4aa323816e911b2ad6be4aedc44abb4fd5f
Brian Cloutier <github@briancloutier.com>
2018-07-08 00:06:53 -0700

    Add notes on constraints, goal decorator

--- logic.py
+++ logic.py
@@ -18,6 +18,81 @@ When you add a new type, it needs to be added to a few places:
     - reify
     - make sure equality works too!
 Is there a way to write code which works for all of these at once?
+
+TODO:
+    Add an event log, so we can return an execution trace?
+    How would you create an interactive debugger?
+    Create a decorator which makes goals easier to write?
+    I like logpy's idea of returning a tuple to represent goal lambdas
+      I think a decorator which wraps goals would be even simpler?
+
+Constraints (http://scheme2011.ucombinator.org/papers/Alvis2011.pdf)
+    - where there was previously an asociation store, there's now:
+      - a domain store
+      - a constraint store (careful that all constraints have at least one variable)
+    - a constraint is (op, *args), where op is the name of the function to call
+    - a domain is a list of natural numbers
+
+    cKanren relies on 3 functions to be available:
+    - process-prefix:      called when a unification happens, updates constraints?
+    - enforce-constraints: runs before reification, checks the constraint store
+    - reify-constraints:   pretty-print the constraint store
+
+    - goal-construct takes a goal and succeeds if constraints successfully apply?
+
+    - when we unify, if we fail or fail to update the store then nothing changes
+      - if we update the store then we call (process-prefix) with the association delta
+        (and return the store that process-prefix returns)
+
+    - run-constraints is a function which re-runs every constraint until fixpoint
+      (you should call it when implementing process-prefix for your constraint system)
+      - that's too slow, so it's passed a list of relevant vars,
+        and only runs the constraints which involve those vars
+
+      - a constraint is a function which takes the current state and returns the next
+        state
+
+    Finite Domain
+    - process-prefix is called when new associations are made and runs on each new one
+      - if the new association is to a value:
+        - if that value is a Var, then intersect the domains
+          - if the domains are disjoint, fail the unification!
+          - if the intersection is a singleton, add it to the substitution
+            - this triggers run-constraints on the Var we just assigned to
+        - if that value is a constant in the domain, nice!
+        - if that value is a constant outside the domain, fail the unification
+      - it then calls run-constraints on the new store
+
+    - enforce-constraints
+      - if we're reifying then we'd prefer to associate our variable with a value, if
+        that's possible (the domain is a singleton), now is the time to do it!
+      - we should also make sure that there exists a solution before reifying, try to
+        force an answer, if that fails then fail reification!
+        (somehow this is specific to the finite domain?)
+
+    Disequality (no domain information is stored, just constraints)
+    - process-prefix just runs the constraints on all new associations
+    - enforce-constraints does nothing, constraints already fail immediately if they're
+      unsatisfiable
+    - reify-constraints returns the list of constraints attached to the specified variable
+
+    - neq is a new goal which takes a left and a right
+      - if left and right are not unifiable then this will never be violated, so no-op
+      - if left and right are unifiable
+        - p is the set of associations which will change when they're unified
+        - if p is empty then your two things are already equal, fail!
+        - okay, p is the set of things which should never become true
+          - add it to the constraint store!
+          - the extra constraint might cause redundency
+            - subsume means: unifying the smaller one with the bigger one causes no change
+          - after adding, drop any subsumed constraints, you don't need them!
+
+    Misc:
+    - unify(pairs, subs) takes a list of new pairs to add and adds them all at once!
+    - test => (\(s) body) means only run the body if (test) succeeds
+      (and run it with the result of (test)
+    - cKanren doesn't support simultanious types of constraints. To do that you need to
+      write your own composite constraint functions
 '''
 
 class ListMeta(type):
@@ -51,6 +126,18 @@ class Var:
 def vars(count):
     # make it convenient to make a few of these at once
     return [Var() for _ in range(count)]
+
+class State:
+    'Stores the current execution state'
+    def __init__(self, subs=None):
+        self.subs = subs if subs is not None else dict()
+
+    def ext_subs(key, value):
+        if occurs(self.subs, key, value):
+            return False
+        newsubs = self.subs.copy()
+        newsubs[key] = value
+        return State(newsubs)
 
 empty_subs = dict()
 
@@ -183,25 +270,44 @@ Okay, a goal returns a lambda which consumes some bindings and emits a stream of
 A stream can be either: None, a pair, or a thunk
 '''
 
-def always():
-    def run(s):
-        while True:
-            yield s
-    return run
+def raw_goal(func):
+    '''
+    A goal is a function which takes:
+        s, a dict of substitutions,
+    And returns a generator which yields all the substitutions for which this goal passes
 
+    raw_goal(func) makes it easier to write goal-returning functions
+        (it's not meant to be called by users)
+        it returns a wrapper which, when called with arguments, returns a goal which,
+        when called with substitutions calls the body of func with an additional kwarg:
+            _s <- the set of substitutions
+        (but only if the goal accepts kwargs, if you don't care about s then it isn't passed)
+    '''
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        def run(s):
+            if '_s' in signature(func).parameters:
+                kwargs['_s'] = s
+            return func(*args, **kwargs)
+        return run
+    return wrapper
+
+@raw_goal
+def always(_s):
+    while True:
+        yield _s
+
+@raw_goal
 def never():
-    def run(s):
-        return
-        yield  # tell Python that this is a generator
-    return run
+    return
+    yield  # tell Python that this is a generator
 
-def eq(left, right):
-    def run(s):
-        result = unify(s, left, right)
-        if result is not False:
-            yield result
-        return
-    return run
+@raw_goal
+def eq(left, right, _s):
+    result = unify(_s, left, right)
+    if result is not False:
+        yield result
+    return
 
 def call_goal(goal, s):
     'A hack to allow recursion. Sometimes the goals are wrapped in lambdas we must unpack'
@@ -216,16 +322,16 @@ def call_goal(goal, s):
 
     return goal(s)
 
-def disj(*goals):
-    # only one of these must be true
-    def run(s):
-        # careful! This is DFS, ala Prolog. We'd probably prefer BFS, ala Kanren
-        for goal in goals:
-            stream = call_goal(goal, s)
-            yield from stream
-    return run
+@raw_goal
+def disj(*goals, _s):
+    # disjunction, provable if any of the goals are provable
+    # careful! This is DFS, ala Prolog. We'd probably prefer BFS, ala Kanren
+    for goal in goals:
+        stream = call_goal(goal, _s)
+        yield from stream
 
-def conj(*goals):
+@raw_goal
+def conj(*goals, _s):
     # every goal must be true
     # say you manage to satisfy the first goal, you must also satisfy the rest with some
       # part of the resulting stream
@@ -240,14 +346,14 @@ def conj(*goals):
         for binding in stream:
             substream = call_goal(first, binding)
             yield from emit(substream, rest)
+            # TODO: all these yield from's lead to an insane stack depth
+            # could we do something trampoline-like and just return the new generator?
 
-    def run(s):
-        first, *rest = goals
-        stream = call_goal(first, s)
+    first, *rest = goals
+    stream = call_goal(first, _s)
 
-        # now, for every part of stream, try to satisfy the rest of the goals with it
-        yield from emit(stream, rest)
-    return run
+    # now, for every part of stream, try to satisfy the rest of the goals with it
+    yield from emit(stream, rest)
 
 # some list helpers
 
@@ -492,6 +598,21 @@ class TestCases(unittest.TestCase):
             ['_0', '_1', '_2', 1, '_3'],
             ['_0', '_1', '_2', '_3', 1, '_4'],
         ])
+
+    def testGoalWrapperWithS(self):
+
+        @raw_goal
+        def uni(left, right, _s):
+            result = unify(_s, left, right)
+            if result is not False:
+                yield result
+
+        x, y = vars(2)
+
+        self.assertEqual(
+            run(1, uni(x, y), x),
+            ['_0']
+        )
 
 if __name__ == '__main__':
     unittest.main()
